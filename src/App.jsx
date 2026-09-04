e7c1961a68facdb74e66b9fdaccd501533f692f1
Abdul Hannan <abdulhannanhere@gmail.com>
2026-09-04 07:57:58 +0500

    Data fetched from unsplashed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import { fetchImages } from "./api/mediaApi";
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-950 text-white'>App</div>
-  )
-}
+    <div className="h-screen w-full bg-gray-950 text-white">
+      <button onClick={() => fetchImages("cat")}>Get Photos</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/api/mediaApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+const PEXEL_KEY = import.meta.env.VITE_PEXELS_KEY;
+
+export const fetchImages = async (query, page = 1, per_page = 20) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    params: { query, page, per_page },
+    headers: {
+      Authorization: `Client-ID ${UNSPLASH_KEY}`,
+    },
+  });
+  console.log(response);
+};
