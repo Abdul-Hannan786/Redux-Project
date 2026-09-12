7b368551e871ef59baefc72363d7950c85743df9
Abdul Hannan <abdulhannanhere@gmail.com>
2026-09-12 06:49:33 +0500

    Collection Slice Created

---
+++ src/redux/slices/CollectionSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: JSON.parse(localStorage.getItem("collection")) || [],
+};
+
+export const collectionSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    addCollection: (state, actions) => {
+      const alreadyExists = state.items.find(
+        (item) => item.id === actions.payload.id,
+      );
+      if (!alreadyExists) {
+        state.items.push(actions.payload);
+        localStorage.setItem("collection", JSON.stringify(state.items));
+      }
+    },
+    removeCollection: (state, actions) => {
+      state.items = state.items.filter((item) => item.id !== actions.payload);
+      localStorage.setItem("collection", state.items);
+    },
+    clearCollection: (state) => {
+      state.items = [];
+      localStorage.removeItem("collection");
+    },
+  },
+});
