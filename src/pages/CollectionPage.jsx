14429ed4fcee245566293d5ec4af54f87b875756
Abdul Hannan <abdulhannanhere@gmail.com>
2026-09-14 18:25:07 +0500

    Update in Collection Page UI

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,25 +1,36 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CollectionCard from "../components/CollectionCard";
+import { addToast, clearCollection } from "../redux/slices/CollectionSlice";
 
 const CollectionPage = () => {
+  const dispatch = useDispatch();
+
   const collection = useSelector((state) => state.collection.items);
   return (
     <div className="px-10 py-6">
-      <div className="flex items-center justify-between mb-5">
-        <h2 className="text-2xl font-semibold">Your Collections</h2>
-        <button className="bg-red-600  px-4 py-1.5 rounded active:scale-95 cursor-pointer transition-all font-medium">
-          Clear Collection
-        </button>
-      </div>
       {collection.length === 0 ? (
-        <div className="flex items-center justify-center w-full h-screen">
-          <h1 className="font-bold text-5xl text-center">No Saved Media</h1>
+        <div className="flex items-center justify-center w-full h-[calc(100vh-156px)]">
+          <h1 className="font-bold text-5xl">Collection is Empty</h1>
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full mx-auto gap-6">
-          {collection?.map((item) => (
-            <CollectionCard item={item} />
-          ))}
+        <div>
+          <div className="flex items-center justify-between mb-5">
+            <h2 className="text-2xl font-semibold">Your Collections</h2>
+            <button
+              onClick={() => {
+                dispatch(clearCollection());
+                dispatch(addToast("Collection Cleared"));
+              }}
+              className="bg-red-600  px-4 py-1.5 rounded active:scale-95 cursor-pointer transition-all font-medium"
+            >
+              Clear Collection
+            </button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full mx-auto gap-6">
+            {collection?.map((item) => (
+              <CollectionCard item={item} />
+            ))}
+          </div>
         </div>
       )}
     </div>
